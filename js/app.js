8f39fdcffbd2ab5f2005146cfeff276de6c06f09
developer-sahadat <mdridoysikder862@gmail.com>
2022-03-01 01:37:58 +0600

    details js code complated

--- js/app.js
+++ js/app.js
@@ -7,16 +7,19 @@
        fetch(url)
        .then(Response=>Response.json())
        .then(data=>products_section(data.data.slice(0,20)))
+       document.getElementById('search-input').value=''
     })
 
     
     // products function code start
 
     const products_section=(phones)=>{
+        
         // all products parent
         const parent=document.getElementById('products-section')
-
+            parent.textContent='';
         phones.forEach(phone => {
+            
             // A div has been created here
             const div=document.createElement('div');
             div.innerHTML=`
@@ -65,19 +68,30 @@
                 
                 <div class="modal-content">
                 <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Phone Details</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                 <div class="d-flex m-2 justify-content-center align-items-center ">
-                <div class="card border-0 shadow-none  w-40">
+                <div class="card border-0 shadow-none  w-50">
                 <img src="${details.data.image}" class=" img-fluied rounded-start" alt="...">
                     
                 </div>
-                <div class="ms-5 w-60">
-                    <h5  >Name: ${details.data.name}</h5>
-                    <h6 >Brand: ${details.data.brand}</h6>
-                     
+                <div class="ms-5 w-50">
+                    <p > <span class='h6 fw-bolder'>Release Date:</span> 
+                    ${details.data.releaseDate}</p>
+                    <p > <span class='h6 fw-bolder'>Chip Set:</span> ${details.data.mainFeatures.chipSet}</p>
+                    <p > <span class='h6 fw-bolder'>Memory:</span> ${details.data.mainFeatures.memory}</p>
+                    <p > <span class='h6 fw-bolder'>Display Size:</span> ${details.data.mainFeatures.displaySize}</p>
+                    <p > <span class='h6 fw-bolder'>Storage:</span> ${details.data.mainFeatures.storage}</p>
+                    <p > <span class='h6 fw-bolder'>Sensors:</span> ${details.data.mainFeatures.sensors}</p>
+                    <p > <span class='h6 fw-bolder'>Bluetooth:</span> ${details.data.others.Bluetooth}</p>
+                    
+                    <p > <span class='h6 fw-bolder'>GPS:</span> ${details.data.others.GPS}</p>
+                    <p > <span class='h6 fw-bolder'>NFC:</span> ${details.data.others.NFC}</p>
+                    <p > <span class='h6 fw-bolder'>NFC:</span> ${details.data.others.Radio}</p>
+                    <p > <span class='h6 fw-bolder'>NFC:</span> ${details.data.others.USB}</p>
+                    <p > <span class='h6 fw-bolder'>NFC:</span> ${details.data.others.WLAN}</p>
                 </div>
                 </div>
                 </div>
