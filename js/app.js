e701a326c300019dd46e6b899184acdff0bed8d9
developer-sahadat <mdridoysikder862@gmail.com>
2022-02-28 23:51:27 +0600

    details style complated

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@
        .then(data=>products_section(data.data.slice(0,20)))
     })
 
+    
     // products function code start
 
     const products_section=(phones)=>{
@@ -16,7 +17,6 @@
         const parent=document.getElementById('products-section')
 
         phones.forEach(phone => {
-            console.log(phone);
             // A div has been created here
             const div=document.createElement('div');
             div.innerHTML=`
@@ -25,13 +25,70 @@
             <div class="card-body">
               <h5 class="card-title fw-bolder py-2">Name: ${phone.phone_name}</h5>
               <h5 class="card-title fw-bolder">Brand: ${phone.brand}</h5>
-              <button href="#" class="custom-button-style my-3">Details Now</button>
+              <button href="#" class="custom-button-style my-3" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="products_details('${phone.slug}')">Details Now</button>
             </div>
           </div>
             `
-            parent.appendChild(div)
+        parent.appendChild(div)
         })
 
     }
 
 /*----------------- Search product code end now--------------------*/
+
+
+
+
+
+/*----------------- product details page code start --------------------*/
+
+    const products_details=id=>{
+        const url=`https://openapi.programming-hero.com/api/phone/${id}`
+        fetch(url)
+        .then(Response=>Response.json())
+        .then(data=>details_page(data))
+    }
+
+
+     // product details page function code start:
+        const details_page=details=>{
+
+            console.log(details);
+
+            const parent=document.getElementById('product-details-page')
+            parent.textContent=""
+
+            // A div has been created here
+            const div=document.createElement('div');
+
+            div.innerHTML=`
+                
+                <div class="modal-content">
+                <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                <div class="d-flex m-2 justify-content-center align-items-center ">
+                <div class="card border-0 shadow-none  w-40">
+                <img src="${details.data.image}" class=" img-fluied rounded-start" alt="...">
+                    
+                </div>
+                <div class="ms-5 w-60">
+                    <h5  >Name: ${details.data.name}</h5>
+                    <h6 >Brand: ${details.data.brand}</h6>
+                     
+                </div>
+                </div>
+                </div>
+                </div>
+
+            `
+            parent.appendChild(div)
+
+     }
+
+
+
+/*----------------- product details page code ends --------------------*/
+
